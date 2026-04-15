b868178582d07f9df438495129cc77b914729b15
Benedict Schönenberger <benedict.schoenenberger@ost.ch>
2026-04-15 09:13:48 +0900

    implemented filter-functionality to projects-page

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,17 @@ let root;
 let iconOpenEye;
 let iconClosedEye;
 let test;
+let projectCards;
 
 
 // DOM aufbauen:
 document.addEventListener("DOMContentLoaded", init);
 
-function init(){
+function init() {
     // we now have access to the DOM tree!
     // set up your initial document event handlers here.
     const themeToggle = document.querySelector("#theme-toggle");
-      if (themeToggle) {
+    if (themeToggle) {
         themeToggle.addEventListener("click", switchTheme);
     } else {
         console.log("Theme Toggle button not found!");
@@ -24,8 +25,8 @@ function init(){
     iconOpenEye = document.querySelector(".icon-eye-light");
     iconClosedEye = document.querySelector(".icon-eye-dark");
 
-    const savedColorScheme = localStorage.getItem("color-scheme"); 
-    if(savedColorScheme === "dark"){
+    const savedColorScheme = localStorage.getItem("color-scheme");
+    if (savedColorScheme === "dark") {
         root.setAttribute("color-scheme", "dark");
         closeEye();
     } else {
@@ -34,20 +35,43 @@ function init(){
 
     test = document.querySelector(".test");
     const filterButtons = document.querySelector(".filter-buttons")
-    
-    function handleClick(event){
-        if(event.target.hasAttribute("value")){
-            console.log("button clicked");
-            test.textContent = `Filter: ${event.target.innerHTML}`;
-        }
-    }       
-
     filterButtons.addEventListener("click", handleClick);
-};
 
-function switchTheme(){
+    projectCards = document.querySelectorAll(".project-card");
+    projectCards.forEach((card) => console.log(card));
+
+}
+
+function handleClick(event) {
+    if (event.target.hasAttribute("value")) {
+        console.log(`button clicked: ${event.target.innerHTML}`);
+        test.textContent = `Filter: ${event.target.innerHTML}`;
+        filterProjects(event.target.innerHTML);
+    }
+}
+
+function filterProjects(category) {
+    // Falls category !== bla -> display:none
+    projectCards.forEach((card) => {
+        const projectCategoryTypes = card.querySelectorAll(".project-category-type");
+        const categories = Array.from(projectCategoryTypes).map(elem => elem.textContent.trim());
+        console.log(categories);
+
+        if (categories.includes(category)){
+                card.classList.remove("hidden");
+                console.log("reached until here, filtering for:" + category)
+            } else {
+
+                card.classList.add("hidden");
+            }
+        }
+
+    )
+}
+
+function switchTheme() {
     console.log("switchTheme called")
-    if(root.getAttribute("color-scheme") !== "dark"){
+    if (root.getAttribute("color-scheme") !== "dark") {
         root.setAttribute("color-scheme", "dark");
         localStorage.setItem("color-scheme", "dark");
         closeEye();
@@ -58,12 +82,12 @@ function switchTheme(){
     }
 }
 
-function openEye(){
+function openEye() {
     iconOpenEye.style.display = 'flex';
     iconClosedEye.style.display = 'none';
 }
 
-function closeEye(){
+function closeEye() {
     iconOpenEye.style.display = 'none';
     iconClosedEye.style.display = 'flex';
 }
